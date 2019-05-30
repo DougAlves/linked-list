9bb3fbf81a1b6f4b283ccd8b88746ce208263f7e
Dougalas Alves <douglas.felipelima18@gmail.com>
2019-05-30 18:56:51 -0300

    quase tudo

---
+++ include/list.h
@@ -0,0 +1,396 @@
+
+#define LINKED_LIST
+
+#include <cstdlib> // size_t
+#include <ostream>
+#include <initializer_list>
+#include<iostream>
+namespace sc
+{
+    template <typename T>
+    class list
+    {
+        private:
+        struct node
+        {
+            T data;
+            node* next;
+            node* prev;
+
+            bool operator==(node & rhs){
+                return data==rhs.data & next == rhs.next && rhs.prev == prev;
+            }
+        };
+
+        public:
+        class iterator
+            {
+                public:
+                    typedef T& reference; //!< Alias para uma referência.
+                    typedef std::ptrdiff_t difference_type;
+                    //! Identificar a categoria do iterador para algoritmos do STL.
+                    typedef std::bidirectional_iterator_tag iterator_category;
+                    //=== Private data
+                private:
+                    //=== Interface pública.
+                public:
+                    node* ptr;
+                    iterator( node * pt = nullptr ) : ptr{ pt }
+                    { /* empty */ }
+                    /// it1 = it2;
+                    iterator& operator=( const iterator& rhs )
+                    {
+                        ptr = rhs.ptr;
+                        return *this;
+                    }
+                    /// *it = x;
+                    T& operator*(void)
+                    {
+                        return ptr->next->data;
+                    }
+                    /// x = *it;
+                    const T& operator*(void) const
+                    {
+                        return ptr->data;
+                    }
+                    /// ++it pré incremento.
+                    iterator operator++(void)
+                    {
+                        //++ptr;
+                        //return *this;
+                        // Retorna um novo iterador que aponta para o próximo endereço.
+
+                        ptr = ptr->next;
+
+                        return iterator(ptr);
+                    }
+                    /// it++ pós incremento.
+                    iterator operator++(int)
+                    {
+                        
+                        iterator temp( ptr ); // Cria um iterator temporário com o endereço atual.
+                        if (ptr->next != nullptr){
+                        ptr = ptr->next;
+
+                        }
+                        return temp;
+                    }
+                    /// --it pré decremento.
+                    iterator operator--(void)
+                    {
+                        return iterator ( ptr->prev );
+                    }
+                    /// it-- pós decremento.
+                    iterator operator--(int)
+                    {
+                        // Opção 1:
+                        iterator temp( ptr ); // Cria um iterator temporário com o endereço atual.
+                        ptr = ptr->prev;
+                        return temp;
+                    }
+                    
+                    /// Comparação it == it2
+                    bool operator==( const iterator &it ) const
+                    { 
+                        //std::cout <<"ENDEREÇO DO THIS: "<< this->ptr<<std::endl;
+                        //std::cout <<"ENDEREÇO DO PARAMETRO : "<<it.ptr << std::endl;
+                        return this->ptr == it.ptr ; /* Devem apontar pro mesmo endereço. */ 
+                    }
+                    /// Comparação it != it2
+                    bool operator!=( const iterator &it ) const
+                    { 
+                        bool equal = *this == it;
+                        //std::cout << equal << std::endl;
+                        //std::cout << !equal <<std::endl;
+                        return  !equal ; /* Não Devem apontar pro mesmo endereço. */ 
+                    }
+                    /// it = it1 - it2;
+                    
+                    iterator operator+( int a  )
+                    {
+                        iterator aux (ptr);
+                        int i = 0;
+                        while (i < a){
+                            aux++;
+                            i++;
+                        }
+                        return aux;
+                    }
+                    iterator operator-( int a  )
+                    {
+                        iterator aux (ptr);
+                        int i = 0;
+                        while (i < a){
+                            aux--;
+                            i++;
+                        }
+                        return aux;
+                    }
+            };
+                
+        private:
+        node* head;
+        node* tail;
+        size_t SIZE;
+        public:
+        list()
+        {
+            head = new node();
+            tail = new node();
+            head->next = tail;
+            tail->prev = head;
+            head->prev = tail->next = nullptr;
+            SIZE = 0;
+        }
+        list( const list &other){
+            head = new node();
+            tail = new node();
+            head->next = tail;
+            tail->prev = head;
+            head->prev = tail->next = nullptr;
+            SIZE = 0;
+            node* it = other.tail->prev;
+            
+            while (it != other.head){
+                this->push_front(it->data);
+                it = it->prev;
+            }
+        }
+
+        list(const std::initializer_list<T>& ilist)
+        {
+            head = new node();
+            tail = new node();
+            head->next = tail;
+            tail->prev = head;
+            SIZE = 0;
+            head->prev = tail->next = nullptr;
+            for (size_t i = 0 ; i + ilist.begin() != ilist.end() ; i++){
+                push_back( *(ilist.begin() + i) );
+            }
+        }
+        
+        list(size_t sz)
+        {
+            head = new node();
+            tail = new node();
+            head->next = tail;
+            tail->prev = head;
+            head->prev = tail->next = nullptr;
+            T e;
+            for (int i =0; i< sz; i++){
+                push_back(e);
+            }
+            SIZE = sz;
+        }
+
+        list(iterator first, iterator last)
+        {
+            head = new node();
+            tail = new node();
+            head->next = tail;
+            tail->prev = head;
+            head->prev = tail->next = nullptr;
+            SIZE = 0;
+            while (first != last){
+                push_back(*first);
+                first++;
+            }
+
+        }
+        ~list(){
+            clear();
+            delete head;
+            delete tail;
+        }
+
+
+        void push_front( const T& data)
+        {
+            node* target = new node();
+            target->data = data;
+            target->prev = head;
+            target->next = head->next;
+            head->next->prev = target;
+            head->next = target;
+            SIZE++;
+        }
+
+        void push_back( const T& data)
+        {
+            node* target = new node();
+            target->data = data;
+            target->next = tail;
+            target->prev = tail->prev;
+            tail->prev->next = target;
+            tail->prev = target;
+            SIZE++;
+        }
+
+        void clear(){
+            if (head->next == tail) return;
+            while (head->next != tail){
+                pop_front();
+            }
+            SIZE = 0;
+        }
+        size_t size()
+        {
+            return SIZE;
+        }
+
+        bool empty(){
+            return  head->next == tail;
+        }
+        
+        void pop_front()
+        {
+            if (head->next == tail) return;
+            node* target = head->next;
+            head->next->next->prev = head;
+            head->next = head->next->next;
+            delete target;
+            SIZE--;    
+        }
+        void pop_back()
+        {
+            if (head->next == tail) return;
+            node* target = tail->prev;
+            tail->prev->prev->next = tail;
+            tail->prev = tail->prev->prev;
+            delete target;
+            SIZE--;
+        }
+
+        void insert(iterator pos, T value)
+        {
+            node* target = new node();
+            target->data = value;
+            target->prev = pos.ptr;
+            target->next = pos.ptr->next;
+            pos.ptr->next->prev = target;
+            pos.ptr->next = target;
+            SIZE++;
+        }
+
+        void insert(iterator pos,iterator first, iterator last )
+        {
+            while (first !=last)
+            {
+                insert(pos, *first++);
+                pos++;
+            }
+        }
+
+        void insert(iterator pos, std::initializer_list<T> ilist)
+        {
+            auto it =  ilist.begin();
+            while (it != ilist.end()){
+                insert(pos, *it);
+                it++;
+                pos++;
+            }
+
+        }
+
+        iterator erase(iterator pos)
+        {
+            node* to_delete = pos.ptr;
+            std::cout <<"Erase" << std::endl;
+            
+            to_delete->prev->next = to_delete->next;
+            to_delete->next->prev =  to_delete->prev;
+            delete to_delete;
+            SIZE--;
+            return pos;
+        }
+
+        iterator erase( iterator first, iterator last)
+        {
+            iterator pos = first;
+            while (first != last){
+            std::cout <<"apagando o :"<<*first << std::endl;
+                erase(first++);
+            }
+            std::cout <<"apagado" << std::endl;
+
+            return pos;
+        }
+
+        const T& back() const{
+            if (head->next == tail) 
+                throw "[]";
+            return tail->prev->data;
+        }
+
+        const T& front() const{
+            if (head->next == tail) 
+                throw "[]";
+            return head->next->data;
+        }
+
+        iterator begin (){
+            return iterator(head);
+        }
+
+        iterator end(){
+            return iterator(tail->prev);
+        }
+
+        list& operator=(list & rhs)
+        {
+            clear();
+            node* it = rhs.head->next;
+            while (it != rhs.tail){
+                push_back(it->data);
+                it = it->next;
+            }
+            return *this;
+
+        }
+
+        list& operator=(const std::initializer_list<T> ilist){
+            clear();
+            auto it = ilist.begin();
+            while (it != ilist.end()){
+                push_back(*it);
+            }
+            return *this;
+        }
+
+        bool  operator==(const list &  rhs){
+            if(rhs.SIZE != SIZE) return false;
+            
+            iterator it (head->next);
+            iterator it2( rhs.head->next) ;
+            while(it != tail)
+            {
+                if (*it != *it2)
+                {
+                    return false;
+                }
+                it ++;
+                it2 ++;
+            }
+            return true;
+        }
+
+        bool operator!=(const list & rhs){
+            return not (*this == rhs);
+        }
+
+        friend std::ostream& operator<<(std::ostream & os, const list &list){
+            node* it;
+            it = list.head->next;
+            os<< "[ ";
+            while(it != list.tail){
+                os << it->data << " ";
+                it = it->next;
+            }
+            os<<"]";
+            return os;
+        };
+
+    };
+
+};
--- src/teste.cpp
+++ src/teste.cpp
@@ -1,27 +1,75 @@
-#include "../include/linkedlist.h"
+#include "../include/list.h"
 #include <iostream>
+#include<cassert>
 int main (){
 
-    ls::list<int> a;
-    ls::list<int> c{1,5,6,7};
 
-    a.push_front(2);
-    a.push_front(45);
-    a.push_back(25);
+        // Aux arrays.
+        sc::list<int> seq1 { 1, 2, 3, 4, 5 };
+        sc::list<int> seq2 { 1, 2, 3, 4, 5 };
+        sc::list<int> source { 6, 7, 8, 9, 10 };
 
-    ls::list<int> b(a);
-    a.pop_front();
-    b.pop_back();
-    b = c;
-    a.clear();
-    try {
-        bool t = c==b;
-        std::cout << "c: "<< c.front() << std::endl;
-        std::cout << "b: "<< b << std::endl;
-        std::cout << (c==b) << std::endl;
-        std::cout << (c != b) << std::endl;
-        std::cout << "a: "<< a.back() << std::endl;
-    }catch(const char* e){
-        std::cout<< e << std::endl;
+        std::cout << ">>> seq1 size is: " << seq1.size() << '\n';
+        // Insert at the begining.
+        seq1.insert( seq1.begin(), source.begin(), source.end() );
+        std::cout << ">>> Seq1 == " << seq1 << std::endl;
+        std::cout << ">>> seq1 size is: " << seq1.size() << '\n';
+        assert( seq1 == ( sc::list<int>{ 6, 7, 8, 9, 10, 1, 2, 3, 4, 5 } ) );
+        std::cout << ">>> Ok\n\n";
+
+        // In the middle
+        seq1 = seq2;
+        seq1.insert(  seq1.begin() + 2 , source.begin(), source.end() );
+        std::cout << ">>> Midele Seq1 == " << seq1 << std::endl;
+        std::cout << ">>> seq1 size is: " << seq1.size() << '\n';
+        assert( seq1 == ( sc::list<int>{ 1, 2, 6, 7, 8, 9, 10, 3, 4, 5 } ) );
+        std::cout << ">>> Ok2\n\n";
+
+        // At the end
+        seq1 = seq2;
+        seq1.insert( seq1.end(), source.begin(), source.end() );
+        std::cout << ">>> Seq1 == " << seq1 << std::endl;
+        std::cout << ">>> seq1 size is: " << seq1.size() << '\n';
+       
+        assert( seq1 == ( sc::list<int>{ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 } ) );
+        std::cout << ">>> Ok3\n\n";
+
+
+        std::cout << ">>> Passed!\n\n";
+
+        {
+        std::cout <<"Erase" << std::endl;
+        // Initial vector.
+        sc::list<int> seq { 1, 2, 3, 4, 5 };
+
+        // removing a segment from the beginning.
+        std::cout <<"entrando na função" << std::endl;
+
+        auto past_last = seq.erase( seq.begin());
+        assert( seq.begin() == past_last );
+        assert( seq == ( sc::list<int>{ 4, 5 } ) );
+        assert( seq.size() == 2 );
+
+        // removing at the middle.
+        seq = { 1, 2, 3, 4, 5 };
+        past_last = seq.erase( seq.begin() + 1, seq.begin() + 4 );
+        assert( seq.begin() + 1 == past_last );
+        assert( seq == ( sc::list<int>{ 1, 5 } ) );
+        assert( seq.size() == 2 );
+
+        // removing a segment that reached the end.
+        seq = { 1, 2, 3, 4, 5 };
+        past_last = seq.erase( seq.begin() + 2 , seq.end() );
+        assert( seq.end() == past_last );
+        assert( seq == ( sc::list<int>{ 1, 2 } ) );
+        assert( seq.size() == 2 );
+
+        // removing the entire vector.
+        seq = { 1, 2, 3, 4, 5 };
+        past_last = seq.erase( seq.begin(), seq.end() );
+        assert( seq.end() == past_last );
+        assert( seq.empty() );
+
+        std::cout << ">>> Passed!\n\n";
     }
 }
